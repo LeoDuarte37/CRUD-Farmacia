361068f8e43c9dbc47fa74151a27c66a6e8d91fa
Leo <leonardo.r.s.duarte@gmail.com>
2024-01-31 12:33:14 -0300

    CRUD-Categoria

---
+++ farmacia/src/main/java/com/generation/farmacia/model/Categoria.java
@@ -0,0 +1,37 @@
+package com.generation.farmacia.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
+@Table(name = "tb_categoria")
+public class Categoria {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long Id;
+	
+	@NotBlank(message = "Atributo nome é de preenchimento obrigatório!")
+	private String nome;
+
+	public Long getId() {
+		return Id;
+	}
+
+	public void setId(Long id) {
+		Id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+}
